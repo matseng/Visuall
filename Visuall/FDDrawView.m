05713d22511c0316e2eac01132f88c65686779a1
Michael Tseng <matseng@gmail.com>
2016-11-11 18:05:16 -0800

    minor improvements to highlight - do to refactor to remove redundancy around hightlightSelectedPath and persist points to firebase and redraw

--- Visuall/FDDrawView.m
+++ Visuall/FDDrawView.m
@@ -70,16 +70,8 @@
     CGContextRef context = UIGraphicsGetCurrentContext();
     CGContextSetLineWidth(context, 0.5f);
     
-    // draw all lines from Firebase
-    //    for (FDPath *path in self.paths) {
-    //        [self drawPath:path withContext:context];
-    //        [self drawPathOnShapeLayer: path withContext: context];
-    
-    //    }
-    
-    // make sure to draw the line the user is currently drawing
-    if (self.currentPath != nil) {
-        //        [self drawPath:self.currentPath withContext:context];
+    if (self.currentPath != nil)
+    {
         [self drawPathOnShapeLayer: self.currentPath withContext: context];
         [self drawPathOnBackgroundLayer: self.currentPath withContext: context];
     }
@@ -167,32 +159,6 @@
 
 - (void) drawPathOnBackgroundLayer:(FDPath *)path withContext:(CGContextRef)context
 {
-//    return;
-    
-    /*
-
-     //Get the CGContext from this view
-     CGContextRef context = UIGraphicsGetCurrentContext();
-     
-     //Set the stroke (pen) color
-     CGContextSetStrokeColorWithColor(context, [UIColor blueColor].CGColor);
-     //Set the width of the pen mark
-     CGContextSetLineWidth(context, 5.0);
-     
-     // Draw a line
-     //Start at this point
-     CGContextMoveToPoint(context, 10.0, 30.0);
-     
-     //Give instructions to the CGContext
-     //(move "pen" around the screen)
-     CGContextAddLineToPoint(context, 310.0, 30.0);
-     CGContextAddLineToPoint(context, 310.0, 90.0);
-     CGContextAddLineToPoint(context, 10.0, 90.0);
-     
-     //Draw it
-     CGContextStrokePath(context);
-     */
-    
     if (path.points.count > 1) {
         // make sure this is a new line
         CGContextBeginPath(context);
@@ -215,7 +181,6 @@
         self.shapeLayerBackground.lineWidth = 10.0;
         [self.shapeLayerBackground setFillColor: nil];
         [self.shapeLayerBackground setPath: pathRef];
-        
     }
 }
 
@@ -325,6 +290,7 @@
     CGPoint touchPoint = [gestureRecognizer locationInView: self];
     [self.currentPath addPoint:touchDownPoint];
     [self.currentPath addPoint:touchPoint];
+    self.selectedPath = self.currentPath;
     [self setNeedsDisplay];
 }
 
